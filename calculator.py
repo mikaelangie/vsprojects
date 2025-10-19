90fe4b96e66c8a995b50e2b7a2b0f5793f3f6980
Ang Mika <mika@Angs-MacBook-Pro.local>
2025-10-19 09:46:58 -0700

    First commit

---
+++ calculator.py
@@ -0,0 +1,7 @@
+x = int(input(' what is x? '))
+y = int(input(' what is y? '))
+
+z=x/y
+
+print(f"{z:.2f}")
+      
---
+++ hello.py
@@ -0,0 +1,4 @@
+def greet(frist_name, last_name):
+    print (f"hi {frist_name} {last_name}")
+    print("welcome aboard")
+    
